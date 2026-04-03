cd16f75ed4be3ddddb09d2b9ae8d3b4519168dbd
Aritra Mallik <aritramallik2002@gmail.com>
2026-04-03 13:51:40 +0530

    Updated settings and ml

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "dasnobita007@gmail.com"
-EMAIL_HOST_PASSWORD = "wkgvvnfhgdvidgkv"
+EMAIL_HOST_USER = "yourgmail@gmail.com"
+EMAIL_HOST_PASSWORD = "abcdefghijklmnop"
 
 #JWT Authentication settings
 REST_FRAMEWORK = {
