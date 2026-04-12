1de37b4f701ec1771d1ed67d14f821a5a0e38f93
Aritra Mallik <aritramallik2002@gmail.com>
2026-04-12 23:40:10 +0530

    Added django-environ and updated requirements.txt

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -13,19 +13,25 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from cryptography.fernet import Fernet
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+
+# Read .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fj8w@u9qfi@@7nhy(&6z^)!4jdwwi-gtgnqez=u@dog^sxlbs#'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = []
 
@@ -149,8 +155,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "yourgmail@gmail.com"
-EMAIL_HOST_PASSWORD = "abcdefghijklmnop"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 #JWT Authentication settings
 REST_FRAMEWORK = {
@@ -185,4 +191,4 @@ CACHES = {
     }
 }
 
-ENCRYPTION_KEY = Fernet.generate_key()
+ENCRYPTION_KEY = env("ENCRYPTION_KEY").encode()
